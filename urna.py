f785eb89a075db1a3c2db2e8bb8c10d41b0d6968
Álvaro Rezende Martimiano <alvarovalim40@gmail.com>
2025-11-25 21:15:03 -0300

    Fiz a parte de ler o arquivo de eleitores

--- urna.py
+++ urna.py
@@ -37,6 +37,46 @@ print("5 - Mostrar resultados")
 print("6 - Fechar programa")
 escolha = int(input("Escolha umas das opções: "))
 
+def carregar_eleitores():
+    dic_eleitores = {}
+    try:
+        # encoding='utf-8' é importante para ler acentos
+        with open("eleitores.txt", "r", encoding="utf-8") as arq:
+            for linha in arq:
+                linha_limpa = linha.strip()
+                if not linha_limpa:
+                    continue
+                
+                dados = linha.split(",")
+                
+                # Previne erro se tiver alguma linha em branco ou incompleta no arquivo
+                if len(dados) >= 5:
+                    nome = dados[0].strip()
+                    rg = dados[1].strip()
+                    titulo = dados[2].strip()
+                    municipio = dados[3].strip()
+                    uf = dados[4].strip()
+                    
+                    # Aqui criamos a entrada no dicionário
+                    dic_eleitores[titulo] = {
+                        "nome": nome,
+                        "rg": rg,
+                        "municipio": municipio,
+                        "uf": uf
+                    }
+        
+        print("Eleitores carregados com sucesso!")
+        return dic_eleitores
+        
+    except Exception as e:
+        print(f"Erro ao carregar eleitores: {e}")
+        return {}
+
 if escolha == 1:
     candidatos = carregar_candidatos()
     print(candidatos)
+
+elif escolha == 2:
+    eleitores = carregar_eleitores()
+    print(eleitores)    
+            
